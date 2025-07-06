f7d27c3af09a988d4df01ffa8f53352b2833db22
Dmitriy <dmitry.nikishin.2021@gmail.com>
2025-07-06 12:38:25 +0300

    Add basic Telegram bot functionality
    - Implement Telegram bot registration and message handling logic.
    - Configure to respond with received message text.
    - Add `Telegram.Bot` package dependency.

--- Gazirovkino.Bot/Program.cs
+++ Gazirovkino.Bot/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
-Console.WriteLine("Hello, World!");
+const string token = ""; //TODO разобраться как не коммитить ключ
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient(token, cancellationToken: cts.Token);
+
+var botUser = await bot.GetMe(); // регистрация нашего бота в Телеграм
+
+bot.OnMessage += OnMessage;
+
+Console.WriteLine($"@{botUser.Username} is running... Press Enter to terminate");
+Console.ReadLine();  // Блокирует выполнение программы, ожидая ввода.
+cts.Cancel();        // Останавливает работу, посылая сигнал для отмены.
+return;
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is null) 
+        return;
+
+    Console.WriteLine($"Received {type} '{msg.Text}' in {msg.Chat}");
+
+    // Ответ в этот же чат с текстом, отправленным пользователем
+    await bot.SendMessage(msg.Chat, $"{msg.From} said: {msg.Text}");
+}
