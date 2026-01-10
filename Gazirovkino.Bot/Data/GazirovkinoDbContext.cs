1791a00b92a800d8c2b2f62e57cf4bb4c087029f
Dima <dmitry.nikishin.2021@gmail.com>
2026-01-10 16:43:37 +0300

    Fix DbContext

--- Gazirovkino.Bot/Data/GazirovkinoDbContext.cs
+++ Gazirovkino.Bot/Data/GazirovkinoDbContext.cs
@@ -14,8 +14,6 @@ public class GazirovkinoDbContext : DbContext
 
     public DbSet<User> Users => Set<User>();
     public DbSet<Survey> Surveys => Set<Survey>();
-    public DbSet<Criteria> Criteria => Set<Criteria>();
-    public DbSet<Interview> Interviews => Set<Interview>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -48,36 +46,6 @@ public class GazirovkinoDbContext : DbContext
             entity.Property(x => x.UserId).IsRequired();
             entity.Property(x => x.DateCreated).IsRequired();
             entity.Property(x => x.Status).IsRequired();
-
-            entity.HasIndex(x => x.UserId);
-            entity.HasOne<User>()
-                .WithMany()
-                .HasForeignKey(x => x.UserId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        modelBuilder.Entity<Criteria>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-            entity.Property(x => x.SurveyId).IsRequired();
-            entity.Property(x => x.DateCreated).IsRequired();
-            entity.Property(x => x.Type).IsRequired();
-            entity.Property(x => x.Status).IsRequired();
-            entity.Property(x => x.Order).IsRequired();
-
-            entity.HasIndex(x => x.SurveyId);
-            entity.HasIndex(x => new { x.SurveyId, x.Order }).IsUnique();
-
-            entity.HasOne<Survey>()
-                .WithMany()
-                .HasForeignKey(x => x.SurveyId)
-                .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        modelBuilder.Entity<Interview>(entity =>
-        {
-            entity.HasKey(x => x.Id);
-            entity.Property(x => x.UserId).IsRequired();
 
             entity.HasIndex(x => x.UserId);
             entity.HasOne<User>()
